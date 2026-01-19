8069f66608802d7b5c0fa171dbdc27e220ed8aad
hugo <HUGOMVERA@GMAIL.COM>
2026-01-19 09:33:59 -0800

    need to add reactive lements to the button

--- index.js
+++ index.js
@@ -1,8 +1,60 @@
-import { registerRootComponent } from 'expo';
+import { registerRootComponent } from "expo";
+import React from "react";
+import { Text, StyleSheet, View, Button, Alert, Platform } from "react-native";
 
-import App from './App';
+let globalCounter = 0;
 
-// registerRootComponent calls AppRegistry.registerComponent('main', () => App);
-// It also ensures that whether you load the app in Expo Go or in a native build,
-// the environment is set up appropriately
+function setCounter() {
+  globalCounter = 1 + globalCounter;
+  showAlert(globalCounter);
+}
+
+function showAlert(message) {
+  if (Platform.OS === "web") {
+    window.alert(message);
+  } else {
+    Alert.alert(message);
+  }
+}
+
+const App = () => {
+  return (
+    <View style={styles.screen}>
+      <Text style={styles.container1}> Hello World </Text>
+      <Text style={styles.container2}>
+        {" "}
+        This is the number of times you have pressd the Button{globalCounter}
+      </Text>
+      <Button
+        onPress={setCounter()}
+        title="Learn More"
+        color="#841584"
+        //    accessibilityLabel="Learn more about this purple button"
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#fff",
+  },
+  container1: {
+    fontWeight: "bold",
+    color: "blue",
+    fontSize: 18,
+  },
+  container2: {
+    fontWeight: "bold",
+    color: "#007AFF",
+    fontSize: 20,
+    marginTop: 10,
+  },
+});
+
+// THIS IS THE KEY LINE:
+// This tells Expo that "App" is the root component of your application.
 registerRootComponent(App);
